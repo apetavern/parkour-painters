08c5c60b301cad56f2973f392448c4bb446ea014
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2023-03-25 17:35:51 -0300

    Add basic entity for dung target

---
+++ code/Entities/GraffitiSpot.cs
@@ -0,0 +1,13 @@
+﻿namespace GangJam;
+
+[Library( "func_graffiti_spot")]
+[Title("Graffiti Spot"), Category("Spray Down")]
+[EditorModel( "models/rust_structures/fences_walls/wall_conc_300x300.vmdl_c" )]
+[HammerEntity]
+public class GraffitiSpot : Entity
+{
+	public override void Spawn()
+	{
+		base.Spawn();
+	}
+}
