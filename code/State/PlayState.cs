09084f686ce711836fdefdbc4d9a7a9f3b6a64c3
peter-r-g <petergorman11@gmail.com>
2023-03-25 19:38:09 -0400

    Add PlayState

---
+++ code/State/PlayState.cs
@@ -0,0 +1,111 @@
+﻿namespace GangJam.State;
+
+/// <summary>
+/// The state for when the main game loop has begun.
+/// </summary>
+internal partial class PlayState : Entity, IGameState
+{
+	/// <summary>
+	/// The active instance of <see cref="PlayState"/>. This can be null.
+	/// </summary>
+	internal static PlayState Instance => GangJam.Current.CurrentState as PlayState;
+	
+	/// <summary>
+	/// Contains all of the clients in team one.
+	/// </summary>
+	[Net] internal IList<IClient> TeamOneClients { get; private set; }
+	/// <summary>
+	/// Contains all of the clients in team two.
+	/// </summary>
+	[Net] internal IList<IClient> TeamTwoClients { get; private set; }
+
+	/// <summary>
+	/// The time in seconds since the game started.
+	/// </summary>
+	[Net] internal TimeSince TimeSinceGameStarted { get; private set; }
+	/// <summary>
+	/// The time in seconds until the game ends.
+	/// </summary>
+	internal TimeUntil TimeUntilGameEnds => GangJam.GameLength - TimeSinceGameStarted;
+
+	/// <inheritdoc/>
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		Transmit = TransmitType.Always;
+	}
+
+	/// <inheritdoc/>
+	void IGameState.Enter( IGameState lastState )
+	{
+		if ( lastState is not WaitingState waitingState )
+		{
+			// TODO: Randomize teams instead.
+			Log.Error( "PANIC" );
+			return;
+		}
+
+		for ( var i = 0; i < waitingState.TeamOne.Length; i++ )
+			TeamOneClients.Add( waitingState.TeamOne[i] );
+
+		for ( var i = 0; i < waitingState.TeamTwo.Length; i++ )
+			TeamTwoClients.Add( waitingState.TeamTwo[i] );
+
+		TimeSinceGameStarted = 0;
+	}
+
+	/// <inheritdoc/>
+	void IGameState.Exit()
+	{
+	}
+
+	/// <inheritdoc/>
+	void IGameState.ClientJoined( IClient cl )
+	{
+		// TODO: Make them a spectator?
+	}
+
+	/// <inheritdoc/>
+	void IGameState.ClientDisconnected( IClient cl, NetworkDisconnectionReason reason )
+	{
+		// TODO: Abandon game if not a spectator?
+	}
+
+	/// <inheritdoc/>
+	void IGameState.ClientTick()
+	{
+		DebugOverlay.ScreenText( $"Time Left: {TimeUntilGameEnds} seconds" );
+
+		var teamOneStart = 2;
+		DebugOverlay.ScreenText( "Team One:", teamOneStart );
+
+		int i;
+		for ( i = 0; i < TeamOneClients.Count; i++ )
+			DebugOverlay.ScreenText( TeamOneClients[i].Name, teamOneStart +  i + 1 );
+
+		var teamTwoStart = i + 2;
+		DebugOverlay.ScreenText( "Team Two:", teamTwoStart );
+
+		for ( var j = 0; j < TeamTwoClients.Count; j++ )
+			DebugOverlay.ScreenText( TeamTwoClients[j].Name, teamTwoStart + j + 1 );	
+	}
+
+	/// <inheritdoc/>
+	void IGameState.ServerTick()
+	{
+		// TODO: Game over state.
+		if ( TimeUntilGameEnds <= 0 )
+			WaitingState.SetActive();
+	}
+
+	/// <summary>
+	/// Sets the <see cref="PlayState"/> as the active state in the game. This can only be called on the server.
+	/// </summary>
+	public static void SetActive()
+	{
+		Game.AssertServer();
+
+		GangJam.Current.SetState<PlayState>();
+	}
+}
