653d441438d6b2edee7233ed7613fe5f2b612df7
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2023-03-25 21:11:17 -0300

    Setup clothing into resources, load dumb for now

--- code/Player/Player.Clothing.cs
+++ code/Player/Player.Clothing.cs
@@ -2,17 +2,30 @@ namespace GangJam;
 
 public partial class Player
 {
-	public ClothingContainer Clothing { get; protected set; }
+	public ClothingContainer ClothingContainer { get; protected set; }
 
 	/// <summary>
 	/// Set the clothes to whatever the player is wearing
 	/// </summary>
 	public void SetupClothing()
 	{
-		Clothing = new();
+		ClothingContainer = new();
 
-		Clothing.ClearEntities();
-		Clothing.LoadFromClient( Client );
-		Clothing.DressEntity( this );
+		var clothes = ResourceLibrary.GetAll<Clothing>();
+
+		foreach ( var clothing in clothes )
+		{
+			if ( PunkClothing.Any( clothing.Title.Contains ) )
+				ClothingContainer.Clothing.Add( clothing );
+		}
+
+		ClothingContainer.DressEntity( this );
 	}
+
+	private static readonly List<string> PunkClothing = new()
+	{
+		"Mohawk",
+		"Punk Jacket",
+		"Punk Jeans",
+	};
 }
