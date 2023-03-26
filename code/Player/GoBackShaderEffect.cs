c6b28f8ba96fcdc80b3d8902fc7fba7b4e82c515
Brian Lindenhof <brian@shadowbrain.net>
2023-03-26 21:06:28 +0200

    Sick visuals

---
+++ code/Player/GoBackShaderEffect.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GangJam;
+
+[SceneCamera.AutomaticRenderHook]
+public partial class GoBackShaderEffect : RenderHook
+{
+	RenderAttributes attributes = new RenderAttributes();
+	Material effectMaterial = Material.Load( "materials/screen.vmat" );
+
+	public override void OnStage( SceneCamera target, Stage renderStage )
+	{
+		if ( renderStage == Stage.AfterUI )
+		{
+			Graphics.GrabFrameTexture( "ColorBuffer", attributes );
+			Graphics.GrabDepthTexture( "DepthBuffer", attributes );
+
+			// Draw our effect material, which is probably using a special post process
+			// shader that uses all the attributes to do cool effects
+			Graphics.Blit( effectMaterial, attributes );
+		}
+	}
+}
