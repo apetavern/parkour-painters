a44c07c4a39e380a7e81749409c422a0a213c45b
Matthew Zegar <matthewzegar@gmail.com>
2023-03-25 18:09:21 -0700

    Fix yeti slav squat

--- code/Player/PlayerAnimator.cs
+++ code/Player/PlayerAnimator.cs
@@ -13,7 +13,7 @@ public partial class PlayerAnimator : EntityComponent<Player>, ISingletonCompone
 		animHelper.WithLookAt( player.EyePosition + player.EyeRotation.Forward * 100.0f, 1.0f, 1.0f, 0.5f );
 		animHelper.AimAngle = player.EyeRotation;
 		animHelper.FootShuffle = 0f;
-		animHelper.DuckLevel = MathX.Lerp( animHelper.DuckLevel, 1 - controller.CurrentEyeHeight.Remap( 30, 72, 0, 1 ).Clamp( 0, 1 ), Time.Delta * 10.0f );
+		animHelper.DuckLevel = MathX.Lerp( animHelper.DuckLevel, player.Tags.Has( "ducked" ) ? 1 : 0, Time.Delta * 10.0f );
 		animHelper.VoiceLevel = (Game.IsClient && cl.IsValid()) ? cl.Voice.LastHeard < 0.5f ? cl.Voice.CurrentLevel : 0.0f : 0.0f;
 		animHelper.IsGrounded = controller.GroundEntity != null;
 		animHelper.IsSwimming = player.GetWaterLevel() >= 0.5f;
