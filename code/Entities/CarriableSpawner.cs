5e3eff8be43e1947cde3ac8d29ccbd2619f9d4f6
Matt944 <matt@ninetyfour.dev>
2023-03-27 21:31:29 +0100

    Create CarriableSpawner - hard coded to give spray can for now.

---
+++ code/Entities/CarriableSpawner.cs
@@ -0,0 +1,37 @@
+﻿namespace GangJam;
+
+[Library( "ent_carriable_spawner" )]
+[Title( "Carriable Spawner" ), Category( "Spray Down" )]
+[EditorModel( "models/entities/spray_paint/spray_paint.vmdl" )]
+[HammerEntity]
+public sealed partial class CarriableSpawner : AnimatedEntity
+{
+	[Net]
+	public TimeSince TimeSinceLastPickup { get; set; }
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		SetModel( "models/entities/spray_paint/spray_paint.vmdl" );
+		Scale = 2f;
+		EnableTouch = true;
+
+		_ = new PickupTrigger() { Position = Position, Parent = this };
+	}
+
+	public override void StartTouch( Entity other )
+	{
+		base.StartTouch( other );
+
+		if ( other is not Player player )
+			return;
+
+		if ( player.CanEquip( typeof( SprayCan ) ) )
+			player.Equip( new SprayCan() );
+		else
+			Log.Info( "Already have one, greedy bugger" );
+
+		TimeSinceLastPickup = 0f;
+	}
+}
