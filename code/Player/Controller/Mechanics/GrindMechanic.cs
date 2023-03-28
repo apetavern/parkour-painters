877944cb8bf887a3de96d5d1d2ba57a94cd19020
Matthew Zegar <matthewzegar@gmail.com>
2023-03-28 10:28:43 -0700

    holy shit

--- code/Player/Controller/Mechanics/GrindMechanic.cs
+++ code/Player/Controller/Mechanics/GrindMechanic.cs
@@ -4,46 +4,31 @@ public class GrindMechanic : ControllerMechanic
 {
 	private GenericPathEntity _path;
 	private int _currentNodeIndex;
-	private float _alpha;
-	private bool _isReverseGrind;
 	private bool _isGrinding = false;
+	private float _alpha;
 
 	protected override bool ShouldStart()
 	{
 		if ( _isGrinding )
 			return true;
 
-		if ( !Input.Pressed( InputButton.Use ) )
-			return false;
-
 		foreach ( var path in Sandbox.Entity.All.OfType<GrindSpot>() )
 		{
 			if ( path.PathNodes.Count < 2 )
 				continue;
 
-			var pa = path.NearestPoint( Controller.Position + Controller.Velocity * Time.Delta, false, out int na, out float ta );
-			var pb = path.NearestPoint( Controller.Position + Controller.Velocity * Time.Delta, true, out int nb, out float tb );
+			var closestNode = path.PathNodes.MinBy( p => Controller.Position.Distance( p.WorldPosition ) );
+			if ( Controller.Position.Distance( closestNode.WorldPosition ) > 30 )
+				continue;
 
-			var dista = pa.Distance( Controller.Position );
-			var distb = pb.Distance( Controller.Position );
+			_currentNodeIndex = path.PathNodes.IndexOf( closestNode );
+			_path = path;
 
-			if ( dista < 30 && (na == 0 || dista < distb) )
-			{
-				_path = path;
-				_currentNodeIndex = na;
-				_alpha = ta;
-				_isReverseGrind = false;
-				return true;
-			}
+			// TODO: debug.
+			if ( _currentNodeIndex == path.PathNodes.Count - 2 )
+				return false;
 
-			if ( distb < 30 && (nb == path.PathNodes.Count - 1 || distb < dista) )
-			{
-				_path = path;
-				_currentNodeIndex = nb;
-				_alpha = tb;
-				_isReverseGrind = true;
-				return true;
-			}
+			return true;
 		}
 
 		return false;
@@ -51,16 +36,27 @@ public class GrindMechanic : ControllerMechanic
 
 	protected override void Simulate()
 	{
+		_alpha += Time.Delta;
 		if ( _currentNodeIndex >= 0 && _currentNodeIndex < _path.PathNodes.Count - 1 )
 		{
+			if ( _alpha >= 1 )
+			{
+				_alpha = 0;
+				_currentNodeIndex += 1;
+				return;
+			}
+
 			_isGrinding = true;
-			var moveHelper = new MoveHelper( _path.PathNodes[_currentNodeIndex].WorldPosition, Controller.Velocity );
-			moveHelper.TryMove( Time.Delta * 0.1f );
 
-			Controller.Position = moveHelper.Position;
-			Controller.Velocity = moveHelper.Velocity;
+			var nextNodeIndex = _currentNodeIndex + 1;
+			var nextPosition = _path.GetPointBetweenNodes( _path.PathNodes[_currentNodeIndex], _path.PathNodes[nextNodeIndex], _alpha );
 
-			_currentNodeIndex += 1;
+			Controller.Velocity = 0;
+			Controller.Position = nextPosition;
+
+			if ( Controller.Player.Position.Distance( nextPosition ) > 0.2f )
+				return;
+
 			return;
 		}
 
