8c66d23f6436e0223c32b7eb259e4646d14822af
peter-r-g <petergorman11@gmail.com>
2023-03-28 21:07:28 -0400

    Stub precaching

---
+++ code/GangJam.Precache.cs
@@ -0,0 +1,12 @@
+﻿namespace GangJam;
+
+partial class GangJam
+{
+	/// <summary>
+	/// Sets up the precache for players joining.
+	/// </summary>
+	private void SetupPrecache()
+	{
+		// TODO: Do precaching here.
+	}
+}
--- code/GangJam.cs
+++ code/GangJam.cs
@@ -17,6 +17,7 @@ public sealed partial class GangJam : GameManager
 		if ( !Game.IsServer )
 			return;
 
+		SetupPrecache();
 		WaitingState.SetActive();
 	}
 
