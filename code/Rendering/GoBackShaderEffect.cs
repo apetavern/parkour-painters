afc225f965cf2f3bf55dfd05dece63596bb570f1
Matthew Zegar <matthewzegar@gmail.com>
2023-03-30 13:11:57 -0700

    UI font, readable chat

--- code/Rendering/GoBackShaderEffect.cs
+++ code/Rendering/GoBackShaderEffect.cs
@@ -9,7 +9,7 @@ internal sealed class GoBackShaderEffect : RenderHook
 	/// <inheritdoc/>
 	public sealed override void OnStage( SceneCamera target, Stage renderStage )
 	{
-		if ( renderStage != Stage.AfterPostProcess )
+		if ( renderStage != Stage.AfterUI )
 			return;
 
 		Graphics.GrabFrameTexture( "ColorBuffer", attributes );
