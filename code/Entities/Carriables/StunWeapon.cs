f8eb74f06445451d7ea34f8ccd8e558a90e33a34
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2023-04-03 22:57:48 -0300

    Remove leftover bodygroup references in StunWeapon

--- code/Entities/Carriables/StunWeapon.cs
+++ code/Entities/Carriables/StunWeapon.cs
@@ -10,16 +10,14 @@ public sealed partial class StunWeapon : BaseCarriable
 
 	protected override string ModelPath => "models/entities/melee_weapons/melee_weapons.vmdl";
 
-	[Net] private int _bodyGroup { get; set; }
+	/// <summary>
+	/// A boolean representation of the next attack anim type (swing at side or overhead)
+	/// </summary>
 	[Net] private bool _holdtypeAttack { get; set; }
 
 	public override void Spawn()
 	{
 		base.Spawn();
-
-		// This should probably be handled from the carriable spawner.
-		_bodyGroup = Game.Random.Int( 0, 2 );
-		SetBodyGroup( "weapontype", _bodyGroup );
 	}
 
 	protected override void OnPrimaryAttack()
