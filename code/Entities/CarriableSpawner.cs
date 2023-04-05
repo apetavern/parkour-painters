bb218061f8f9e7f4699508d3ec7a3eb047fd1688
Matthew Zegar <matthewzegar@gmail.com>
2023-04-05 11:02:14 -0700

    Change alpha to be zero so weapons appear as unavailable

--- code/Entities/CarriableSpawner.cs
+++ code/Entities/CarriableSpawner.cs
@@ -56,7 +56,7 @@ internal sealed partial class CarriableSpawner : AnimatedEntity
 	/// <summary>
 	/// The alpha component of the render color when the spawner is unavailable.
 	/// </summary>
-	private const float UnavailableAlpha = 0.6f;
+	private const float UnavailableAlpha = 0f;
 
 	/// <inheritdoc/>
 	public sealed override void Spawn()
