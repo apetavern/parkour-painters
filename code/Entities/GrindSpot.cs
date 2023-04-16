38f2b63e713814716907184abada2c7a40f6d55c
Matthew Zegar <matthewzegar@gmail.com>
2023-04-15 20:12:07 -0700

    Setup basic replacement point

--- code/Entities/GrindSpot.cs
+++ code/Entities/GrindSpot.cs
@@ -1,11 +1,107 @@
 namespace ParkourPainters.Entities;
 
+public struct GrindPoint
+{
+	public Vector3 Position;
+	public Vector3 TangentIn;
+	public Vector3 TangentOut;
+}
+
 /// <summary>
 /// A spot that a palyer can grind on.
 /// </summary>
 [Library( "grind_spot" )]
 [Title( "Grind Spot" ), Category( "Movement" )]
 [HammerEntity, Path( "path_generic_node" )]
-internal sealed class GrindSpot : GenericPathEntity
+public partial class GrindSpot : GenericPathEntity
 {
+	private readonly List<GrindPoint> _grindPoints = new();
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		CreateGrindPoints();
+	}
+
+	public override void ClientSpawn()
+	{
+		base.Spawn();
+
+		CreateGrindPoints();
+	}
+
+	private void CreateGrindPoints()
+	{
+		if ( PathNodes.Count < 2 )
+			return;
+
+		_grindPoints.Clear();
+
+		var firstNode = PathNodes[0];
+		var lastNode = PathNodes[1];
+
+		_grindPoints.Add( new GrindPoint() { Position = firstNode.WorldPosition, TangentIn = firstNode.WorldTangentIn, TangentOut = firstNode.WorldTangentOut } );
+		_grindPoints.Add( new GrindPoint() { Position = lastNode.WorldPosition, TangentIn = lastNode.WorldTangentIn, TangentOut = lastNode.WorldTangentOut } );
+	}
+
+	public void DrawGrindSpot()
+	{
+		for ( var nodeid = 0; nodeid < _grindPoints.Count; nodeid++ )
+		{
+			var node = _grindPoints[nodeid];
+
+			Vector3 nodePos = node.Position;
+
+			// Nodes & IDs
+			DebugOverlay.Sphere( nodePos, 4, Color.White );
+			DebugOverlay.Text( $"{nodeid + 1}", nodePos + Vector3.Up * 6, Color.White, 0, 2500 );
+
+			Vector3 nodeTanIn = node.TangentIn;
+			DebugOverlay.Sphere( nodeTanIn, 2, Color.Yellow );
+			DebugOverlay.Line( nodePos, nodeTanIn, Color.Yellow );
+
+			Vector3 nodeTanOut = node.TangentOut;
+			DebugOverlay.Sphere( nodeTanOut, 6, Color.Orange );
+			DebugOverlay.Line( nodePos, nodeTanOut, Color.Orange );
+
+			if ( nodeid + 1 >= _grindPoints.Count )
+				continue;
+
+			var nodeNext = _grindPoints[nodeid + 1];
+
+			for ( int i = 1; i <= 30; i++ ) // Starting from i = 1 because i = 0 is start.Position
+			{
+				var lerpPos = GetPointBetweenNodes( node, nodeNext, (float)i / 30 );
+
+				DebugOverlay.Line( nodePos, lerpPos, Color.Green );
+
+				nodePos = lerpPos;
+			}
+		}
+	}
+
+	private Vector3 GetPointBetweenNodes( GrindPoint start, GrindPoint end, float t, bool reverse = false )
+	{
+		Vector3 tanSrc = reverse ? start.TangentIn : start.TangentOut;
+		Vector3 tanTgt = reverse ? end.TangentOut : end.TangentIn;
+
+		return Vector3.CubicBeizer( start.Position, end.Position, tanSrc, tanTgt, t );
+	}
+
+	[ConCmd.Admin( "regen" )]
+	private static void Regen()
+	{
+		foreach ( var path in Sandbox.Entity.All.OfType<GrindSpot>() )
+			path.CreateGrindPoints();
+
+		ClientRegen();
+	}
+
+	[ClientRpc]
+	public static void ClientRegen()
+	{
+		foreach ( var path in Sandbox.Entity.All.OfType<GrindSpot>() )
+			path.CreateGrindPoints();
+	}
 }
--- code/Entities/Player/Controller/Mechanics/GrindMechanic.cs
+++ code/Entities/Player/Controller/Mechanics/GrindMechanic.cs
@@ -2,101 +2,18 @@ namespace ParkourPainters.Entities;
 
 public partial class GrindMechanic : ControllerMechanic
 {
-	[Net, Predicted] private GenericPathEntity _path { get; set; }
-	[Net, Predicted] private int _currentNodeIndex { get; set; }
-	[Net, Predicted] private bool _isGrinding { get; set; }
-	[Net, Predicted] private float _alpha { get; set; }
-	[Net, Predicted] private bool _isReverse { get; set; }
-	private TimeSince _timeSinceExit { get; set; }
-
-	private float _incomingSpeed;
-
 	protected override bool ShouldStart()
 	{
-		if ( _isGrinding )
-			return true;
-
-		if ( _timeSinceExit < 0.5f )
-			return false;
-
-		if ( Controller.GroundEntity != null )
-			return false;
-
 		foreach ( var path in Sandbox.Entity.All.OfType<GrindSpot>() )
 		{
 			if ( path.PathNodes.Count < 2 )
 				continue;
 
-			var closestNode = path.PathNodes.OrderBy( p => Controller.Position.Distance( p.WorldPosition ) ).First();
-			if ( Controller.Position.Distance( closestNode.WorldPosition ) > 50 )
-				continue;
+			path.DrawGrindSpot();
 
-			if ( Controller.Position.z < closestNode.WorldPosition.z )
-				continue;
-
-			var directionToFirstNode = path.PathNodes[0].WorldPosition - closestNode.WorldPosition;
-			var dot = Vector3.Dot( Player.Rotation.Forward, directionToFirstNode );
-
-			// Lerp the player to the starting node to make the grind seem smoother.
-			Player.Position = Vector3.Lerp( Controller.Position, closestNode.WorldPosition, Time.Delta );
-
-			_isReverse = dot > 0;
-			_currentNodeIndex = path.PathNodes.IndexOf( closestNode );
-			_path = path;
-			return true;
+			return false;
 		}
 
 		return false;
-	}
-
-	protected override void Simulate()
-	{
-		if ( _currentNodeIndex < 0 || _path is null || _path.PathNodes is null || _currentNodeIndex >= _path.PathNodes.Count )
-		{
-			Stop();
-			return;
-		}
-
-		_isGrinding = true;
-
-		var increment = _isReverse ? -1 : +1;
-		var currentNode = _path.PathNodes[_currentNodeIndex];
-		var nextNodeIndex = _currentNodeIndex + increment;
-		var nextNode = _path.PathNodes.ElementAtOrDefault( nextNodeIndex ) ?? currentNode;
-		var distanceBetweenNodes = currentNode.WorldPosition.Distance( nextNode.WorldPosition );
-
-		var nextPosition = _path.GetPointBetweenNodes( currentNode, nextNode, _alpha, _isReverse );
-		var speed = Math.Max( 300f, _incomingSpeed );
-
-		_alpha += Time.Delta * (speed / distanceBetweenNodes);
-		if ( _alpha >= 0.98f )
-		{
-			_alpha = 0;
-			_currentNodeIndex += increment;
-		}
-
-		Controller.Velocity = (nextPosition - Controller.Position).Normal * speed;
-		Controller.Position = Vector3.Lerp( Controller.Position, nextPosition, Time.Delta );
-		Player.Rotation = Controller.Velocity.Normal.EulerAngles.WithPitch( 0 ).ToRotation();
-
-		if ( Input.Pressed( InputButton.Jump ) )
-		{
-			Player.JumpMechanic.Start();
-			Stop();
-		}
-	}
-
-	protected override void OnStart()
-	{
-		_incomingSpeed = Player.Velocity.Length;
-	}
-
-	protected override void OnStop()
-	{
-		IsActive = false;
-		_isGrinding = false;
-		_timeSinceExit = 0;
-
-		Player.PlaySound( "grind_exit" );
 	}
 }
