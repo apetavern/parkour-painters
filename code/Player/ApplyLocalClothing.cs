202f28425ccf88a573e6e14804f222f0b99c335b
Jammie3d <60801006+Jammie3d@users.noreply.github.com>
2024-03-19 02:38:19 +0000

    Put some clothes on Terry for now

---
+++ code/Player/ApplyLocalClothing.cs
@@ -0,0 +1,12 @@
+using Sandbox;
+
+public sealed class ApplyLocalClothing : Component
+{
+	[Property] public SkinnedModelRenderer BodyTarget { get; set; }
+
+	protected override void OnAwake()
+	{
+		var clothing = ClothingContainer.CreateFromLocalUser();
+		clothing.Apply( BodyTarget );
+	}
+}
